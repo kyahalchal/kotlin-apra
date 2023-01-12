5f482f83c8c60f625c53190a9fbf1c34f1db978b
kyahalchal <abhinavs@apra.in>
2023-01-12 13:32:26 +0530

    program completed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 import java.io.File
+//import java.nio.file.Paths
 
+//method to diplay content of program
 fun catF(filePath: String): Unit {
     val srcFile = File(filePath)
     for (line in srcFile.readLines()){
@@ -7,36 +9,69 @@ fun catF(filePath: String): Unit {
     }
 }
 
-fun copyF(SourcePath: String, DestPath: String): Unit {
-    val srcFile =  File(SourcePath)
-    val destFile =  File(DestPath)
+//method to copy content of a given file to another file
+fun copyF(srcPath: String, destPath: String): Unit {
+    val srcFile =  File(srcPath)
+    val destFile =  File(destPath)
     for (line in srcFile.readLines()){
         destFile.appendText(line+"\n")
     }
 }
 
+//method to display frequency of each words in a given file
 fun countWord(filePath: String): Unit {
     val srcFile = File(filePath)
-    var wordFreq = LinkedHashMap<String, Int>()
-    for (line in srcFile.readLines()){
-        var lineSplit = line.split(" ")
+    var wordFreq  = mutableMapOf<String, Int>()
 
+    //stores key and value in map
+    for (line in srcFile.readLines()){
+        var lineSplit = line.split(" ",",",". ",";",":","/","\n","?","\\","<",">","#",
+            "{","}","[","]","\"","!","@","$","%","^","&","*","(",")","\t","\r",".")
+        for (word in lineSplit){
+            if (word in wordFreq){
+                var count = wordFreq.getValue(word)
+                wordFreq[word] = count+1
+            }else{
+                wordFreq[word] = 1
+            }
+        }
     }
 
+    wordFreq.remove("")  //removes mapping of empty string
+
+    //display map
+    for (key in wordFreq.keys) {
+        println("$key = ${wordFreq[key]}")
+    }
 }
 
 fun main() {
-//    val sc = Scanner(System.`in`)
+//    val path = Paths.get("").toAbsolutePath().toString()
+
+
+    val path = System.getProperty("user.dir")
     do {
-        println("Choose: ")
-        var command = readln()
-        var commandSplit = command.split(" ")
+        println(path)
+        var command = readln()  //example - cat filepath.txt
+        var commandSplit = command.split(" ") //["cat", "filepath.txt"]
 
         when (commandSplit[0]){
-            "cat" -> catF(commandSplit[1])
-            "copy" -> copyF(commandSplit[1],commandSplit[2])
-            "exit" -> print("Thanks for coming!")
-            "cw" -> countWord(commandSplit[1])
+            "cat" -> try{
+                        catF(commandSplit[1])
+                    }catch (e: java.io.FileNotFoundException){
+                        println("File not found")
+                    }
+            "copy" -> try {
+                        copyF(commandSplit[1],commandSplit[2])
+                    }catch (e: java.io.FileNotFoundException){
+                        println("File not found")
+                    }
+            "wc" -> try {
+                        countWord(commandSplit[1])
+                    }catch (e: java.io.FileNotFoundException){
+                        println("File not found")
+                    }
+            "exit" -> print("Exiting...")
             else -> print("Invalid Query")
         }
     }while (commandSplit[0] != "exit")
